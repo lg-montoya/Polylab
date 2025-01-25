7975b4d6c359c1c85902756149063593671c6231
lg-montoya <lgmontoya@gmail.com>
2025-01-25 15:01:57 -0500

    Fixed UI elements
    Dashboard now looks as planned. Controls will be added.

--- tabs.py
+++ tabs.py
@@ -18,39 +18,42 @@ tab_0 = html.Div([
                 dbc.Col(
                     dbc.Button("Instructions", color="info", outline=False,id='xx'), class_name='mb-3')
                 ),
-
+            # ROW containing ALL the controls. This is subdivided itself into 2 columns. The first column has THREE rows.
             dbc.Row([
                 dbc.Col([
-                    html.Div(dcc.Dropdown(id='dropdown_menu_1', options=[{'label': key, 'value': key} for key in POLYNOMIALS.keys()], 
-                                          clearable=False, placeholder='CHOOSE A POLYNOMIAL', className="mb-4 mt-2")),
-                    html.Div(dcc.Markdown(mathjax=True, id='eq_1'), style={"textAlign": "center"}),
-                    
-                    
-                    dbc.Card([ 
-                        dbc.Row([
-                            dbc.Label(html.Div(dcc.Markdown("$$\quad a$$",mathjax=True)), width='1'),
-                            dbc.Col(dcc.Slider(id="slider_1_a",min=-5, max=5, value=1,marks={i: str(i) for i in range(-5, 6)}, className="mt-4 mb-4",), width=11),
-                        ]),
-                        dbc.Row([
-                            dbc.Label(html.Div(dcc.Markdown("$$\quad b$$",mathjax=True)), width=1),
-                            dbc.Col(dcc.Slider(id="slider_1_b",min=-5, max=5, value=1,marks={i: str(i) for i in range(-5, 6)}, className="mb-4"), width=11),
-                        ]),
-                        dbc.Row([
-                            dbc.Label(html.Div(dcc.Markdown("$$\quad c$$",mathjax=True)), width=1),
-                            dbc.Col(dcc.Slider(id="slider_1_c",min=-5, max=5, value=1,marks={i: str(i) for i in range(-5, 6)}, className="mb-4"), width=11),
-                        ]),
-                        dbc.Row([
-                            dbc.Label(html.Div(dcc.Markdown("$$\quad d$$",mathjax=True)), width=1),
-                            dbc.Col(dcc.Slider(id="slider_1_d",min=-5, max=5, value=1,marks={i: str(i) for i in range(-5, 6)}), width=11),
-                        ])
-                    ]),
-                ], width=4),
+                    # Row for the dropdown menu
+                    dbc.Row(html.Div(dcc.Dropdown(id='dropdown_menu_1', options=[{'label': key, 'value': key} for key in POLYNOMIALS.keys()], 
+                                    clearable=False, placeholder='CHOOSE A POLYNOMIAL'))),
+                    # Row for the general form of the polynomial in LaTeX
+                    dbc.Row(html.Div(dcc.Markdown("&nbsp;", mathjax=True, id='eq_1'), style={"textAlign": "center"})),
+                    # Row for the sliders
+                    dbc.Row(    
+                        dbc.Card([    
+                            dbc.Row([
+                                dbc.Label(html.Div(dcc.Markdown("$$\quad a$$",mathjax=True)), width=1),
+                                dbc.Col(dcc.Slider(disabled=True,id="slider_1_a",min=-5, max=5, value=1,marks={i: str(i) for i in range(-5, 6)}, className="mt-4 mb-4",), ),
+                            ]),
+                            dbc.Row([
+                                dbc.Label(html.Div(dcc.Markdown("$$\quad b$$",mathjax=True)), width=1),
+                                dbc.Col(dcc.Slider(disabled=True,id="slider_1_b",min=-5, max=5, value=1,marks={i: str(i) for i in range(-5, 6)}, className="mb-4"), ),
+                            ]),
+                            dbc.Row([
+                                dbc.Label(html.Div(dcc.Markdown("$$\quad c$$",mathjax=True)), width=1),
+                                dbc.Col(dcc.Slider(disabled=True,id="slider_1_c",min=-5, max=5, value=1,marks={i: str(i) for i in range(-5, 6)}, className="mb-4"), ),
+                            ]),
+                            dbc.Row([
+                                dbc.Label(html.Div(dcc.Markdown("$$\quad d$$",mathjax=True)), width=1),
+                                dbc.Col(dcc.Slider(disabled=True,id="slider_1_d",min=-5, max=5, value=1,marks={i: str(i) for i in range(-5, 6)}), ),
+                            ])
+                        ], style={'width': '100%', 'height': '100%'})    
+                    ),
+                ], width=4, style={'display': 'flex', 'flexDirection': 'column', 'justifyContent': 'space-between'}),
 
                 
                 dbc.Col([
                     html.Div(
-                        dbc.Card(dbc.CardBody(dcc.Graph(figure=poly.plot(), id='tab-0-graph')), ))
-                    ], width=8, class_name="mt-2"),
+                        dbc.Card(dcc.Graph(figure=poly.plot(), id='tab-0-graph')), )
+                    ], width=8),
     
     
             ]), 
