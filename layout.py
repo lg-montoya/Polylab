f68415c499fc23f75a99f94cb65956f1324f3c0d
lg-montoya <lgmontoya@gmail.com>
2025-04-04 15:41:40 -0500

    Cosmetics: Introduction
     Asterix comments removed. To be reimplemented at a later stage.

--- layout.py
+++ layout.py
@@ -18,17 +18,20 @@ def app_layout(default_theme, other_theme):
         dbc.Container(
             children=[
                 # html.Div([fluid_mode_switch, theme_switch], className="d-flex justify-content-end"),
+                
                 html.Div(fluid_mode_switch, className="d-flex justify-content-end"), 
                 html.Div(theme_switch, className="d-flex justify-content-end"), 
-            
-                
+ 
                 dbc.Tabs([                  
                     dbc.Tab(introduction.tab, id='tab-introduction', label='Introduction'),
                     dbc.Tab(polynomials.tab, id='tab-polynomials', label='Polynomials'),
                     # dbc.Tab(id='tab-sinusoidals', label='Sinusoidals', children=[sinusoidals.tab]),
                 ], id='tab-group', class_name='nav-stack')
+                
             ], id='main-container', fluid=False, class_name="mt-3 dbc"
         )
     ])
     
     return layout
+
+
