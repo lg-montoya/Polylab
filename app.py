1443f584ef3543b463e8042d40c67283e9f5050c
lg-montoya <lgmontoya@gmail.com>
2025-04-13 21:39:54 -0500

    Upgrade
     Moved from python 3.12.8 to 3.12.9.
    
     Moved to dash 3.0.
    
     Changed requirements file.

--- app.py
+++ app.py
@@ -43,6 +43,6 @@ callbacks_cosmetics.callback_wrapper(app, chart_default_theme, chart_other_theme
 # Set the server to run in production or development mode
 if __name__ == '__main__':
     if FLASK_DEBUG == 'development':
-        app.run_server(debug=True, threaded=True, dev_tools_hot_reload=True, port=8080)
+        app.run(debug=True, threaded=True, dev_tools_hot_reload=True, port=8080)
     elif FLASK_DEBUG == 'production':
-        app.run_server(debug=False)
+        app.run(debug=False)
