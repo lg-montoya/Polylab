8bfc821b9cbb4f1ec95b95dc174ce1f9f9b1674d
lg-montoya <lgmontoya@gmail.com>
2025-09-27 17:13:52 +0100

    Cosmetic: more ruff formatting applied

--- tabs/sinusoidals.py
+++ tabs/sinusoidals.py
@@ -4,20 +4,37 @@ from modals import MODAL_SINUSOIDAL_INSTRUCTIONS
 from defaults.mathematics import SINUSOIDALS
 
 
-tab = html.Div([
-            # ROW with just the instructions and "+" button
-            dbc.Row([
-                dbc.Col([                    
-                    html.Div([
-                    dbc.Button("Instructions", color="info", outline=False,id='btn-mdl-instructions-sinusoidals-open'),
-                    dbc.Button("+", color="secondary", id='dynamic-add-sinusoidal-btn', className='me-md-2')
+tab = html.Div(
+    [
+        # ROW with just the instructions and "+" button
+        dbc.Row(
+            [
+                dbc.Col(
+                    [
+                        html.Div(
+                            [
+                                dbc.Button(
+                                    "Instructions",
+                                    color="info",
+                                    outline=False,
+                                    id="btn-mdl-instructions-sinusoidals-open",
+                                ),
+                                dbc.Button(
+                                    "+",
+                                    color="secondary",
+                                    id="dynamic-add-sinusoidal-btn",
+                                    className="me-md-2",
+                                ),
+                            ],
+                            className="d-grid gap-2 d-md-flex justify-content-md-start",
+                        ),
+                        MODAL_SINUSOIDAL_INSTRUCTIONS,
                     ],
-                    className="d-grid gap-2 d-md-flex justify-content-md-start",    
-                    ),
-                    MODAL_SINUSOIDAL_INSTRUCTIONS,
-                ], class_name='mb-3')
-            ]),
-            
-            dbc.Row(id='dynamic-sinusoidal',children=[])
-                       
-], className="mt-4")
+                    class_name="mb-3",
+                )
+            ]
+        ),
+        dbc.Row(id="dynamic-sinusoidal", children=[]),
+    ],
+    className="mt-4",
+)
